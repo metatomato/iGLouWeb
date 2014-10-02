9c4dfcc2472f5cff6413c7a6426d9c8c1fcd861c
metatomato <metatomato@iglou.gl>
2014-10-02 08:01:56 +0200

    studio setup

--- studio/views.py
+++ studio/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 
 def home_view(request):
-	return HttpResponse("<html>Hola señor metaTomato!! Como està a la casa?</html>")
+    return render(request,'base.html')	
+

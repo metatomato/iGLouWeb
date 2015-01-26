d65271ce109c56a7b1435ece28a55b9d82b80a5d
metatomato <metatomato@iglou.gl>
2015-01-26 16:47:22 +0100

    Add Studio Body Footer & Header Tags

--- studio/views.py
+++ studio/views.py
@@ -10,6 +10,7 @@ def view_home(request):
     return render(request, 'base.html', {"body_header": view_studio_body_header(),
                                          "body_tag": get_navbar_body_tag(),
                                          "body_content": view_studio_body_content(),
+                                         "body_footer": view_studio_body_footer(),
                                          "includes_content": view_studio_includes(),
                                          "scripts_content": view_studio_scripts()
                                          })
@@ -17,14 +18,17 @@ def view_home(request):
 
 def view_studio_body_header():
     content = get_navbar_body()
+    content += get_studio_body_header()
     return content
-
 
 def view_studio_body_content():
     content = get_news_body()
     content += get_studio_body_content()
     return content
 
+def view_studio_body_footer():
+    content = get_studio_body_footer()
+    return content
 
 def view_studio_includes():
     content = get_navbar_includes()
@@ -37,10 +41,21 @@ def view_studio_scripts():
     #content += get_news_scripts()
     return content
 
+
+
+
 def get_studio_body_content():
     content = render_to_string('studio_body.html')
     return content
 
 def get_studio_body_scripts():
     content = render_to_string('studio_scripts.html')
+    return content
+
+def get_studio_body_header():
+    content = render_to_string('studio_body_header.html')
+    return content
+
+def get_studio_body_footer():
+    content = render_to_string('studio_body_footer.html')
     return content

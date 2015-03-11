a3283ead53635190253e3a8e2bffb92173c12e47
metatomato <metatomato@iglou.gl>
2015-03-11 16:01:22 +0100

    Add Legals Contents

--- studio/views.py
+++ studio/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.template.loader import render_to_string
-from navbar.views import view_navbar,get_navbar_body,get_navbar_body_tag,get_navbar_includes,get_navbar_scripts
-from news.views import get_news_body,get_news_includes,get_news_scripts
+from navbar.views import view_navbar, get_navbar_body, get_navbar_body_tag, get_navbar_includes, get_navbar_scripts
+from news.views import get_news_body, get_news_includes, get_news_scripts
 
 # Create your views here.
 
@@ -13,7 +13,7 @@ def view_home(request):
                                          "body_footer": view_studio_body_footer(),
                                          "includes_content": view_studio_includes(),
                                          "scripts_content": view_studio_scripts()
-                                         })
+    })
 
 
 def view_studio_body_header():
@@ -21,14 +21,17 @@ def view_studio_body_header():
     content += get_studio_body_header()
     return content
 
+
 def view_studio_body_content():
     content = get_news_body()
     content += get_studio_body_content()
     return content
 
+
 def view_studio_body_footer():
     content = get_studio_body_footer()
     return content
+
 
 def view_studio_includes():
     content = get_navbar_includes()
@@ -36,13 +39,12 @@ def view_studio_includes():
     content += get_studio_includes()
     return content
 
+
 def view_studio_scripts():
     content = get_navbar_scripts()
     content += get_studio_body_scripts()
-    #content += get_news_scripts()
+    # content += get_news_scripts()
     return content
-
-
 
 
 def get_studio_body_content():
@@ -50,8 +52,10 @@ def get_studio_body_content():
     content_framework = render_to_string('studio_body_framework.html')
     content_about = render_to_string('studio_body_about.html')
     content_contact = render_to_string('studio_body_contact.html')
-    content = content_services + content_framework + content_about + content_contact
+    content_legals = render_to_string('studio_body_legals.html')
+    content = content_services + content_framework + content_about + content_contact + content_legals
     return content
+
 
 def get_studio_body_scripts():
     content_services = render_to_string('studio_scripts.html')
@@ -64,13 +68,16 @@ def get_studio_body_scripts():
 
     return content
 
+
 def get_studio_body_header():
     content = render_to_string('studio_body_header.html')
     return content
 
+
 def get_studio_body_footer():
     content = render_to_string('studio_body_footer.html')
     return content
+
 
 def get_studio_includes():
     content = render_to_string('studio_includes.html')

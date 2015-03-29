1c6b4009cd90468b7ada05213e5596ee8ed7e8c5
metatomato <metatomato@iglou.gl>
2015-03-29 14:16:35 +0200

    Fixed contact script html format

--- studio/studio_request.py
+++ studio/studio_request.py
@@ -1,9 +1,7 @@
 from django.utils.html import format_html
 
 def get_script_request(request):
-    content = ''
     script = ''
     if(request == 'contact'):
-        content = '$(function(){gotoContact();});'
-        script = '<script type="text/javascript">%s</script>' % content
-    return format_html(u"{0}", script)
+        script = '$(function(){gotoContact();});'
+    return format_html(u"<script type='text/javascript'>{0}</script>", script)
